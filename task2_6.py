cea324ea54977002099f24716b41d79ce5ccbf63
artsygankov <vslet@rambler.ru>
2022-07-03 13:04:17 +0300

    Еще не завершено

--- task2_6.py
+++ task2_6.py
@@ -1,10 +1,21 @@
 # Program to create structure and some analytics
-count_list = 0
-count_tuple = 0
 count_dict = 0
-my_structure = [({})]
+goods_dict = {}
+goods_tuple = ()
+goods_list = []
+analytics_dict = {}
 while True:
-    users_input = input(f'Введите значение элемента списка, для завершения введите STOP или stop, и нажмите Enter: ')
-    if users_input == 's' or users_input == 'S':
+    good_name = input(' Введите, пожалуйста, наименование товара: ')
+    good_price = input(' Введите, пожалуйста, цену товара: ')
+    good_quantity = input(' Введите, пожалуйста, количество этого товара: ')
+    good_measure = input(' Введите, пожалуйста, единицу измерения товара: ')
+    goods_dict = {'Наименовние': good_name, 'Цена': good_price, 'Количество': good_quantity,
+                  'Единица измерения': good_measure}
+    goods_tuple = (count_dict + 1, goods_dict)
+    goods_list.append(goods_tuple)
+    users_input = input(
+        'Для продолжения ввода нажмите Enter, для завершения, введите STOP или stop и нажмите Enter: ')
+    if users_input == 'stop' or users_input == 'STOP':
         break
-    my_structure[count_list].
+    count_dict = +1
+print(goods_list)
